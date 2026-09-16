2f4aaf613e7c74cff1a57a75db40a5ab8f93d9ca
snxyz <snxyz@example.com>
2026-09-16 10:12:35 +0000

    feat: add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,8 @@
 def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Возвращает список словарей с операциями, у которых ключ state равен переданному значению."""
     return [operation for operation in operations if operation.get("state") == state]
+
+
+def sort_by_date(operations: list[dict], reverse: bool = True) -> list[dict]:
+    """Возвращает список словарей с операциями, отсортированный по дате."""
+    return sorted(operations, key=lambda operation: operation["date"], reverse=reverse)
